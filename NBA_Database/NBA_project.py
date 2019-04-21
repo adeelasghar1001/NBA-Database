0ff877c1a63d03413a02e572df97be0178d8784f
ev3024 <47435859+ev3024@users.noreply.github.com>
2019-04-21 17:46:16 -0400

    Update NBA_project.py

--- NBA_Database/NBA_project.py
+++ NBA_Database/NBA_project.py
@@ -11,15 +11,50 @@ def printTeam(x):
             print(sheet.cell_value(i,j), end=" | ")
         print()
 
-#location of file
-loc = "C:\\Users\\micha\\OneDrive\\Desktop\\Central.xlsx"
+def EditPlayer(x):
+    playerEdit = int(input("Choose a player to edit (Enter rank): "))
+    statEdit = input("What stat would you like to edit? ")
+    statEdit = statEdit.upper() #convert string to caps for ease of finding stat
+    if (statEdit == "TRB"):
+        sEdit = 2
+    elif (statEdit == "AST"):
+        sEdit = 3
+    elif (statEdit == "STL"):
+        sEdit = 4
+    elif (statEdit == "BLK"):
+        sEdit = 5
+    elif (statEdit == "TOV"):
+        sEdit = 6
+    elif (statEdit == "PTS/G"):
+        sEdit = 7
+    else:
+        print("Error. Not a valid stat.")
+        return
 
-wb = xlrd.open_workbook(loc)
-sheet = wb.sheet_by_index(0) # For central 0 = Bucks, 1 = Bulls, 2 = Cavs, 3 = Pacers, 4 = Pistons
+    nEdit = float(input("Enter new stat: ")) 
+    x.write(playerEdit,sEdit, nEdit)
+
+def showMenu():
+    print("\nWhat would you like to do? \n1. Add Player \n2. Edit Player \n3. Remove Player \n4. Back To Main Menu")
+    
+#location of file
+loc = "C:\\Users\\micha\\OneDrive\\Desktop\\Central.xls"
+
+rb = xlrd.open_workbook(loc) # open excel file
+
+wb = copy(rb) # make writable copy of excel file
+
+# read the sheet to write to within the writable copy
+sheet = wb.get_sheet(0) # For central 0 = Bucks, 1 = Bulls, 2 = Cavs, 3 = Pacers, 4 = Pistons
 
 #print information
 print("Milwaukee Bucks Player Stats")
-printTeam(sheet)
+# printTeam(rb)
 
-print("\nWhat would you like to do? \n1. Add Player \n2. Edit Player \n3. Remove Player \n4. Back To Main Menu")
-c = input("Select an option: ")
+
+showMenu()
+c = int(input("Select an option: "))
+
+if (c == 2):
+    EditPlayer(sheet)
+    wb.save(loc)
